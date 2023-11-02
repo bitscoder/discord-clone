c7d7264314c34e92f7d16978756d08e4392eefa3
bitscoder <ruturajchintawar93@gmail.com>
2023-11-01 18:17:19 -0700

    Hello From 1Nov

---
+++ components/providers/modal-provide.tsx
@@ -0,0 +1,18 @@
+"use client"
+import { CreateSeverModal } from "../modals/create-server-modal";
+import { useEffect, useState } from "react";
+
+export const ModalProvider = () => {
+    const [isMounted, setIsMounted] = useState(false);
+    useEffect(()=>{
+        setIsMounted(true);
+    },[]);  
+    if(!isMounted){
+        return null;
+    }
+    return(
+        <>
+            <CreateSeverModal/>
+        </>
+    )
+}   
