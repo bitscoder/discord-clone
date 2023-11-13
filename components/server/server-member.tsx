c7b0d41a83525847dbf08c7d29fcbecdcf31c293
bitscoder <ruturajchintawar93@gmail.com>
2023-11-12 17:55:13 -0800

    before socket

--- components/server/server-member.tsx
+++ components/server/server-member.tsx
@@ -23,8 +23,12 @@ export const ServerMember = ({
     const parmas = useParams();
     const router = useRouter();
     const icon = roleIconMap[member.role];
+    const onClick = () => {
+        router.push(`/servers/${parmas.serverId}/conversations/${member.id}`);
+    }
     return (
         <button
+        onClick={onClick}
         className={cn(
             "group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1",
             parmas?.memberId === member.id && "bg-zinc-700/20 dark:bg-zinc-700"
@@ -36,7 +40,7 @@ export const ServerMember = ({
                 <p
                 className={cn(
                     "font-semibold text-sm text-zinc-500 group-hover:text-zinc-600 dark:text-zinc-400 dark:group-hover:text-zinc-300 transition",
-                    parmas?.channelId === member.id && "text-primary dark:text-zinc-200 dark:group-hover:text-white"
+                    parmas?.memberId === member.id && "text-primary dark:text-zinc-200 dark:group-hover:text-white"
                 )}>
                     {member.profile.name}
                 </p>
